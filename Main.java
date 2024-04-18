23bcf7fcf1865a55191269b7fcf5b9a6ac0d9405
ForRealy <tucorreo@gmail.com>
2024-04-18 20:40:01 +0200

    menu metodo

--- Main.java
+++ Main.java
@@ -6,15 +6,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
         boolean exit = false;
-        //exit = true;
 
         while (!exit) {
-            System.out.println("Benvingut al joc!");
-            System.out.println("1. Nova partida");
-            System.out.println("2. Carregar partida");
-            System.out.println("3. Configuració");
-            System.out.println("4. Sortir");
-
+            gameMenu();
             System.out.print("Selecciona una opció: ");
             int option = sc.nextInt();
 
@@ -35,20 +29,34 @@ public class Main {
                     errorGame();
                     break;
             }
-        }sc.close();
+        }
+        sc.close();
     }
+
+    public static void gameMenu() {
+        System.out.println("Benvingut al joc!");
+        System.out.println("1. Nova partida");
+        System.out.println("2. Carregar partida");
+        System.out.println("3. Configuració");
+        System.out.println("4. Sortir");
+    }
+
     public static void newGame() {
         System.out.println("Has seleccionat Nova partida.");
     }
+
     public static void saveGame() {
         System.out.println("Has seleccionat Carregar partida.");
     }
+
     public static void configure() {
         System.out.println("Has seleccionat Configuració.");
     }
+
     public static void exitGame() {
         System.out.println("Has seleccionat Sortir. Fins aviat!");
     }
+
     public static void errorGame() {
         System.out.println("Opció invàlida. Torna a intentar.");
     }
