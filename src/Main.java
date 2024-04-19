72567d6a45bcbf2ea0960933aedb6c20c3826668
00-KOGA <amoralesalcaide@elpuig.xeill.net>
2024-04-19 17:43:34 +0200

    Clase TUI creada. Método Main ligéramente cambiado

--- src/Main.java
+++ src/Main.java
@@ -7,56 +7,27 @@ public class Main {
         sc.useLocale(Locale.ENGLISH);
 
         while (true) {
-            gameMenu();
+            TUI.gameMenu();
             System.out.print("Selecciona una opció: ");
             int option = sc.nextInt();
 
             switch (option) {
                 case 1:
-                    newGame();
+                    TUI.newGame();
                     break;
                 case 2:
-                    saveGame();
+                    TUI.saveGame();
                     break;
                 case 3:
-                    configure();
+                    TUI.configure();
                     break;
                 case 4:
-                    exitGame();
+                    TUI.exitGame();
                     break;
                 default:
-                    errorGame();
+                    TUI.errorGame();
                     break;
             }
         }
-        //sc.close();
-    }
-
-    public static void gameMenu() {
-        System.out.println("Benvingut al joc!");
-        System.out.println("1. Nova partida");
-        System.out.println("2. Carregar partida");
-        System.out.println("3. Configuració");
-        System.out.println("4. Sortir");
-    }
-
-    public static void newGame() {
-        System.out.println("Has seleccionat Nova partida.");
-    }
-
-    public static void saveGame() {
-        System.out.println("Has seleccionat Carregar partida.");
-    }
-
-    public static void configure() {
-        System.out.println("Has seleccionat Configuració.");
-    }
-
-    public static void exitGame() {
-        System.out.println("Has seleccionat Sortir. Fins aviat!");
-    }
-
-    public static void errorGame() {
-        System.out.println("Opció invàlida. Torna a intentar.");
     }
 }
---
+++ src/TUI.java
@@ -0,0 +1,33 @@
+public class TUI{
+    public static void gameMenu() {
+        System.out.println("Benvingut al joc!");
+        System.out.println("1. Nova partida");
+        System.out.println("2. Carregar partida");
+        System.out.println("3. Configuració");
+        System.out.println("4. Sortir");
+    }
+
+    public static void newGame() {
+        System.out.println("Has seleccionat Nova partida.");
+    }
+
+    public static void saveGame() {
+        System.out.println("Has seleccionat Carregar partida.");
+    }
+
+    public static void configure() {
+        System.out.println("Has seleccionat Configuració.");
+    }
+
+    public static void exitGame() {
+        System.out.println("Has seleccionat Sortir. Fins aviat!");
+    }
+
+    public static void errorGame() {
+        System.out.println("Opció invàlida. Torna a intentar.");
+    }
+}
+
+
+
+
